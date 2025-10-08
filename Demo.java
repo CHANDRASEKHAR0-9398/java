8575a99e1f871e7d64e6a0c58b9280e82f5848bc
CHANDRASEKHAR0-9398 <vanachandrasekhar38@gmail.com>
2025-10-08 10:18:37 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+class Demo{
+	public static void main(String args[]){
+		try{
+			int a=10;
+			int b=0;
+			int c=a/b;
+			System.out.println(c);
+			}
+		catch(ArithmeticException e){
+			System.out.println(e);
+			}
+		finally{
+		System.out.println("program completed");
+		}
+	}
+}
